4399110ea7065532a531dc0443df4c0b2f3f2d19
payne-chris-r <06cpayne@gmail.com>
2016-09-22 15:43:13 -0400

    Show shortcut for routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
+
+
   resources :users, only: [:index, :show]
 
-  get '/books', to: 'books#index'
-  get '/books/:id', to: 'books#show'
-  post '/books', to: 'books#create'
-  patch '/books/:id', to: 'books#update'
-  delete 'books/:id', to: 'books#destroy'
+  resources :books
+  # same thing ^
+  # get '/books', to: 'books#index'
+  # get '/books/:id', to: 'books#show'
+  # post '/books', to: 'books#create'
+  # patch '/books/:id', to: 'books#update'
+  # delete 'books/:id', to: 'books#destroy'
 end
