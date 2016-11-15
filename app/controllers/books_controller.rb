fb1a20b6d3c0332d43eb6c6c942445b4b2a65442
racheltstevens <racheltstevens@gmail.com>
2016-11-15 10:51:00 -0500

    Implement create method on Books controller
    Tested with curl

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,13 @@
 class BooksController < ApplicationController
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author)
+  end
+
   def index
     @books = Book.all
 
@@ -7,5 +16,15 @@ class BooksController < ApplicationController
 
   def show
     render json: Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      head :no_content
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
   end
 end
